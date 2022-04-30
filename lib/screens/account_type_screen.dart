f7d2d01c874d37517b8978f020dcb49dbcf8333c
Luis Urruela <lurruela@aptude.com>
2022-04-30 15:55:08 -0500

    UI re-design for account type screen

--- lib/screens/account_type_screen.dart
+++ lib/screens/account_type_screen.dart
@@ -6,57 +6,123 @@ class AccountType extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Select your account type')),
-      body: Center(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 150,
-            height: 150,
-            child: TextButton(
-              onPressed: () => Navigator.pushNamed(context, 'login'),
-              child: const Text(
-                'Parent',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.blue),
-              ),
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.blue.shade50,
-              boxShadow: const [
-                BoxShadow(color: Colors.lightBlue, spreadRadius: 3),
-              ],
-            ),
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            child: TextButton(
-                onPressed: () {},
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+              gradient: RadialGradient(
+                  center: Alignment(-0.3, -0.95),
+                  radius: 0.8,
+                  colors: [
+                Color.fromRGBO(255, 130, 205, 1),
+                Color.fromRGBO(62, 40, 201, 1),
+              ])),
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
                 child: const Text(
-                  'Kid',
+                  'Select your user type',
                   style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.blue),
-                )),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.blue.shade50,
-              boxShadow: const [
-                BoxShadow(color: Colors.lightBlue, spreadRadius: 3),
-              ],
-            ),
-          ),
-        ],
-      )),
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: const Text(
+                  'Your permissions will be created based on this selection',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w300),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: 160,
+                child: TextButton(
+                  onPressed: () => Navigator.pushNamed(context, 'login'),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "I'm a parent",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 30,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "I’ll be managing the account",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 14,
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: const Color.fromRGBO(50, 32, 161, 1),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: 160,
+                child: TextButton(
+                    onPressed: () {},
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "I'm a Kid",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 30,
+                              color: Colors.white),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "You will win points by completing tasks",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 14,
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    )),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: const Color.fromRGBO(50, 32, 161, 1),
+                ),
+              ),
+            ],
+          )),
+        ),
+      ),
     );
   }
 }
