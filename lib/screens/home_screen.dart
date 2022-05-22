456df79f8a83e24dc723e4013104eef16c9e50c7
Luis Urruela <lurruela@aptude.com>
2022-05-22 01:30:59 -0500

    Added UI to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:habitos/screens/email_verification_screen.dart';
 import 'package:habitos/screens/signin_screen.dart';
 
+import '../theme/theme.dart';
+
 class HomeScreen extends StatelessWidget {
   final currentUser = FirebaseAuth.instance.currentUser;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -14,14 +16,6 @@ class HomeScreen extends StatelessWidget {
           child: Scaffold(
             key: _scaffoldKey,
             drawer: const sidebar(),
-            appBar: AppBar(
-              title: const Text('Kids Habits'),
-              automaticallyImplyLeading: false,
-              leading: IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: () => _scaffoldKey.currentState!.openDrawer(),
-              ),
-            ),
             body: HomeWidget(currentUser: currentUser),
           ),
           onWillPop: () async {
@@ -31,48 +25,167 @@ class HomeScreen extends StatelessWidget {
 }
 
 class HomeWidget extends StatelessWidget {
-  const HomeWidget({
-    Key? key,
-    required this.currentUser,
-  }) : super(key: key);
+  const HomeWidget({Key? key, required this.currentUser}) : super(key: key);
 
   final User? currentUser;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          Container(
-            width: 200,
-            height: 200,
-            child: TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'add-kid');
-                },
-                child: const Icon(
-                  Icons.add,
-                  color: Color.fromARGB(255, 204, 204, 204),
-                  size: 80,
-                )),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              color: Colors.grey.shade50,
-              boxShadow: const [
-                BoxShadow(
-                    color: Color.fromARGB(255, 214, 214, 214), spreadRadius: 3),
-              ],
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Stack(
+      children: [
+        Column(
+          children: [
+            SingleChildScrollView(
+              child: Container(
+                height: height,
+                width: width,
+                decoration: AppTheme.backgroundGradient,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, top: 10),
+                      child: Column(
+                        children: [
+                          AppBar(
+                              backgroundColor: Colors.transparent,
+                              elevation: 0,
+                              leading: ElevatedButton(
+                                child: const Text(
+                                  'M',
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                onPressed: () =>
+                                    Scaffold.of(context).openDrawer(),
+                                style: ElevatedButton.styleFrom(
+                                    primary:
+                                        const Color.fromRGBO(255, 102, 104, 1),
+                                    side: const BorderSide(
+                                        width: 2, color: Colors.white),
+                                    elevation: 3,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    padding: const EdgeInsets.all(10)),
+                              ),
+                              title: const Text('Habity'),
+                              actions: <Widget>[
+                                Container(
+                                  margin: const EdgeInsets.only(right: 15),
+                                  child: Padding(
+                                      child: IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.notifications),
+                                        iconSize: 35,
+                                      ),
+                                      padding: const EdgeInsets.all(0)),
+                                )
+                              ]),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Row(
+                            children: const [
+                              Text(
+                                'Hi there,',
+                                style: TextStyle(
+                                    color: Color.fromARGB(228, 255, 255, 255),
+                                    fontSize: 25),
+                              ),
+                              Text('👋', style: TextStyle(fontSize: 25))
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              RichText(
+                                  text: const TextSpan(
+                                      text: 'Melissa ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w800,
+                                          fontSize: 30),
+                                      children: <TextSpan>[
+                                    TextSpan(
+                                        text: 'has',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w500)),
+                                    TextSpan(
+                                        text: ' 0 points',
+                                        style: TextStyle(
+                                          color: AppTheme.tertiary,
+                                          fontWeight: FontWeight.w800,
+                                          fontSize: 30,
+                                        )),
+                                    TextSpan(
+                                        text: ' in total',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w500))
+                                  ])),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Center(
+                            child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            Container(
+                              width: 200,
+                              height: 200,
+                              child: TextButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, 'add-kid');
+                                  },
+                                  child: const Icon(
+                                    Icons.add,
+                                    color: Colors.white54,
+                                    size: 80,
+                                  )),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: AppTheme.primary,
+                                boxShadow: const [
+                                  BoxShadow(
+                                      color: Colors.white54, spreadRadius: 3),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 50,
+                            ),
+                          ],
+                        )),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(40),
+                                topRight: Radius.circular(40)),
+                            color: AppTheme.primary),
+                        width: width,
+                        height: height,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
