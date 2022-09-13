f2ce6d747d37a4771261fcf40ec837cda08faf48
Luis Urruela <lurruela@aptude.com>
2022-09-13 16:11:15 -0500

    Fix habity card items alignment

--- lib/screens/home/habits_widget.dart
+++ lib/screens/home/habits_widget.dart
@@ -54,7 +54,6 @@ class HabityComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
-    print(data);
     return Column(
       children: [
         Container(
@@ -62,48 +61,56 @@ class HabityComponent extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(
-                IconData(data['icon'], fontFamily: 'HabityIcons'),
-                color: AppTheme.tertiary,
-                size: 30,
+              SizedBox(
+                width: width * 0.12,
+                child: Icon(
+                  IconData(data['icon'], fontFamily: 'HabityIcons'),
+                  color: Color(data['color']),
+                  size: 30,
+                ),
               ),
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        data['title'],
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w900,
-                            color: Colors.white,
-                            fontSize: 18),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Text(
-                        'Go for it!',
-                        style:
-                            TextStyle(color: AppTheme.tertiary, fontSize: 18),
-                      ),
-                    ],
+              SizedBox(
+                width: width * 0.54,
+                child: Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['title'],
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w900,
+                              color: Colors.white,
+                              fontSize: 18),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text(
+                          'Go for it!',
+                          style:
+                              TextStyle(color: AppTheme.tertiary, fontSize: 18),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-              Row(children: [
-                const Icon(
-                  Icons.star,
-                  color: AppTheme.secondary,
-                  size: 20,
-                ),
-                Text(
-                  '+${data['points'].toString()}',
-                  style: const TextStyle(color: Colors.white, fontSize: 16),
-                )
-              ])
+              SizedBox(
+                  width: width * 0.14,
+                  child: Row(children: [
+                    const Icon(
+                      Icons.star,
+                      color: AppTheme.secondary,
+                      size: 20,
+                    ),
+                    Text(
+                      '+${data['points'].toString()}',
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                    )
+                  ])),
             ],
           ),
           decoration: const BoxDecoration(
